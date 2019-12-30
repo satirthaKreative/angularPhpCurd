8daf7b7f6258930bb41ee002a18b525a25180831
Satirtha Kreative <satirthaKreative@gmail.com>
2019-12-30 11:33:50 +0530

    Dec 30,2019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
